696a3483e58c495d6d1f34d9584312968fd360b4
JBasias <jbasias@gmail.com>
2023-12-12 18:29:34 -0500

    a needed commit

--- merge-sort.js
+++ merge-sort.js
@@ -1,6 +1,24 @@
 // Merge Sort out-of-place
 // Do not modify the original array
-function mergeSort(arr) {
+function mergeSort(A) {
+
+
+  if (A.length <= 1) return A;
+
+  const mid = Math.floor(A.length / 2);
+  let L = A.slice(0, mid);
+  let R = A.slice(mid);
+
+  L = mergeSort(L);
+  R = mergeSort(R);
+  //const sortedL = mergeSort(L);
+  //const sortedR = mergeSort(R);
+
+  return merge(L,R);
+
+  // return merge(sortedL, sortedR);
+
+
 
   // Check if the input is length 1 or less
     // If so, it's already sorted: return
@@ -18,6 +36,44 @@ function mergeSort(arr) {
 // Takes in two sorted arrays and returns them merged into one
 function merge(arrA, arrB) {
 
+
+
+  let ret =[];
+
+  let i=0; let j=0; let n=arrA.length; let m=arrB.length;
+
+
+  while(i<n || j<m)
+  {
+     if(i === n)
+     {
+      ret.push(arrB[j]);
+      j++;
+     }
+     else if(j === m)
+     {
+       ret.push(arrA[i]);
+       i++;
+     }
+     else
+     {
+       if(arrA[i]<arrB[j])
+       {
+         ret.push(arrA[i]);
+         i++;
+       }
+       else
+       {
+         ret.push(arrB[j]);
+         j++;
+       }
+     }
+
+  }
+
+  return ret;
+
+
   // Create an empty return array
 
   // Point to the first value of each array
@@ -31,4 +87,3 @@ function merge(arrA, arrB) {
 }
 
 module.exports = [merge, mergeSort];
-
